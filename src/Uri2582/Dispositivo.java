9352a045053f631a0890bb3e0166fdcd0a717e2b
jefeg <jefeg@PC-Jeferson>
2017-12-03 22:00:13 -0200

    Corrigido sistema e adicionado Uri2582

---
+++ src/Uri2582/Dispositivo.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Uri2582;
+
+/**
+ *
+ * @author jefeg
+ */
+public class Dispositivo {
+    
+    public void botoes (int num1, int num2){
+        int soma = num1 + num2;
+        
+        switch (soma){
+            case 0:
+                System.out.print("PROXYCITY");
+                break;
+            case 1:
+                System.out.print("P.Y.N.G.");
+                break;
+            case 2:
+                System.out.print("DNSUEY!");
+                break;
+            case 3:
+                System.out.print("SERVERS");
+                break;
+            case 4:
+                System.out.print("HOST!");
+                break;
+            case 5:
+                System.out.print("CRIPTONIZE");
+                break;
+            case 6:
+                System.out.print("OFFLINE DAY");
+                break;
+            case 7:
+                System.out.print("SALT");
+                break;
+            case 8:
+                System.out.print("ANSWER!");
+                break;
+            case 9:
+                System.out.print("RAR?");
+                break;
+            case 10:
+                System.out.print("WIFI ANTENNAS");
+                break;
+        }
+    }
+}
+    
+
